764a93eb749b279cfbab76f1b30c6c7a47519ba6
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-02-03 17:50:45 +0100

    WIP

--- js/script.js
+++ js/script.js
@@ -7,16 +7,68 @@ $(document).ready(function(){
       'month' : 0
     },
     'success' : function(data){
-
-      calendarGen();
-      var cane = isSameData(data, moment('2018-01-01'));
-      console.log(cane);
-
+      var firstMonth = moment('2018').startOf('year');
+      var monthDays = moment(firstMonth).daysInMonth();
+      var same = isSameData(data, firstMonth);
+      console.log(same);
+      // stampo il primo del mese
+      var day = moment(firstMonth).format('D');
+      var month = moment(firstMonth).format('MMMM');
+      if (same == true) {
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        var monthD = {
+          'DD' : day,
+          'MMMM' : month
+          // 'FESTIVITY' :
+        };
+        var html = template(monthD);
+        $('.month').append(html)
+      } else {
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        var monthD = {
+          'DD' : day,
+          'MMMM' : month,
+          // 'FESTIVITY' : data.response
+        };
+        var html = template(monthD);
+        $('.month').append(html)
+      }
+      // stampo il resto dei giorni
+      for(var i = 1; i < monthDays; i++){
+        firstMonth = moment(firstMonth, "YYYY-MMMM-D").add(1, 'days');
+        same = isSameData(data, firstMonth);
+        console.log(same);
+        day = moment(firstMonth).format('D');
+        month = moment(firstMonth).format('MMMM');
+        if (same == true) {
+          var source = $("#entry-template").html();
+          var template = Handlebars.compile(source);
+          var monthD = {
+            'DD' : day,
+            'MMMM' : month
+          };
+          var html = template(monthD);
+          $('.month').append(html);
+        } else {
+          var source = $("#entry-template").html();
+          var template = Handlebars.compile(source);
+          var monthD = {
+            'DD' : day,
+            'MMMM' : month
+          };
+          var html = template(monthD);
+          $('.month').append(html);
+        }
+      }
     },
     'error' : function(request, state, errors){
       alert(errors);
     }
   });
+
+  // funzione per verificare la data nella chiamata
 
   function isSameData(date1Obj, date2){
     var length = date1Obj.response.length;
@@ -29,39 +81,15 @@ $(document).ready(function(){
     return result;
   }
 
-
-
-
-  function calendarGen(){
-    var firstMonth = moment('2018').startOf('year');
-    var monthDays = moment(firstMonth).daysInMonth();
-
-    // stampo il primo del mese
-    var day = moment(firstMonth).format('D');
-    var month = moment(firstMonth).format('MMMM');
-    var source = $("#entry-template").html();
-    var template = Handlebars.compile(source);
-    var monthD = {
-      'DD' : day,
-      'MMMM' : month
-    };
-    var html = template(monthD);
-    $('.month').append(html)
-
-    // stampo il resto dei giorni
-    for(var i = 1; i < monthDays; i++){
-      firstMonth = moment(firstMonth, "YYYY-MMMM-D").add(1, 'days');
-      day = moment(firstMonth).format('D');
-      month = moment(firstMonth).format('MMMM');
-
-      var source = $("#entry-template").html();
-      var template = Handlebars.compile(source);
-      var monthD = {
-        'DD' : day,
-        'MMMM' : month
-      };
-      var html = template(monthD);
-      $('.month').append(html);
+  function whichFestivity(date1Obj, date2){
+    var length = date1Obj.response.length;
+    var i = 0;
+    var result = false;
+    while (i < length && result == false){
+      result = moment(date1Obj.response[i].date).isSame(date2, 'day');
+      i++;
     }
+    return result;
   }
+
 });
