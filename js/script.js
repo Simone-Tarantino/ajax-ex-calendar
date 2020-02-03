cd12bfe175852c1494c7a3525fbf694402f93250
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-02-03 16:53:28 +0100

    Ulteriore cambio di rotta

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-  calendarGen();
   $.ajax({
     'url' : 'https://flynn.boolean.careers/exercises/api/holidays',
     'method' : 'GET',
+    'data' : {
+      'year' : 2018,
+      'month' : 0
+    },
     'success' : function(data){
+
+      calendarGen();
 
     },
     'error' : function(request, state, errors){
@@ -11,30 +16,41 @@ $(document).ready(function(){
     }
   });
 
-  // function calendarGen(){
-  //   var firstDMonth = moment([2018, 0, 1]);
-  //   var lastDMonth = moment([2018, 1, 1]);
-  //   var monthLength = lastDMonth.diff(firstDMonth, 'days');
-  //   for(var i = 1; i <= monthLength; i++){
-  //     var day = moment(firstDMonth).format('D');
-  //     var source = $("#entry-template").html();
-  //     var template = Handlebars.compile(source);
-  //     var dayObj = {
-  //       'day' : day,
-  //       'month' : moment(firstDMonth).format('MMMM')
-  //     };
-  //     var html = template(dayObj);
-  //     $('.month').append(html);
-  //   }
-  // }
+  function isSameData(date1, date2){
+    var result = moment(date1).isSame(date2, 'day');
+    return result;
+  }
+
+
+
 
   function calendarGen(){
-    var firstMonth = moment([2018, 0, 1]);
-    console.log(firstMonth);
+    var firstMonth = moment('2018').startOf('year');
     var monthDays = moment(firstMonth).daysInMonth();
-    console.log(monthDays);
-    for(var i = 0; i < monthDays; i++){
+    var day = moment(firstMonth).format('D');
+    var month = moment(firstMonth).format('MMMM');
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var monthD = {
+      'DD' : day,
+      'MMMM' : month
+    };
+    var html = template(monthD);
+    $('.month').append(html)
 
+    for(var i = 1; i < monthDays; i++){
+      firstMonth = moment(firstMonth, "YYYY-MMMM-D").add(1, 'days');
+      day = moment(firstMonth).format('D');
+      month = moment(firstMonth).format('MMMM');
+
+      var source = $("#entry-template").html();
+      var template = Handlebars.compile(source);
+      var monthD = {
+        'DD' : day,
+        'MMMM' : month
+      };
+      var html = template(monthD);
+      $('.month').append(html);
     }
   }
 });
